01d7f6e2b6b5a6a41de610e138ac621ab9f4e780
Julien Lair <julien.lair@gmail.com>
2020-06-09 20:48:54 +0200

    finished project

--- ZCasino.py
+++ ZCasino.py
@@ -1,42 +1,58 @@
 # coding: utf-8
-import math
 import random
+import math
 
-# Wallet = 50$
-# Player bets on number (0 - 49)
-# Roll ball
-# Compare result with bet
-# if result_nb == bet_nb then wallet = wallet + bet + bet * 3
-# elif result_color == bet_color then wallet = walllet + bet + bet * 0.5
+wallet = 100
+continue_game = True
 
-def nb_color (nb):
-    if nb%2 == 0:
-        return 'black'
+print ("Welcome to the ZCasino !\nYou have ${}".format(wallet))
+
+while continue_game:
+    bet_nb = -1
+    while bet_nb < 0 or bet_nb > 49:
+        bet_nb = input ("Which number do you bet on (0-49)? ")
+        try:
+            bet_nb = int(bet_nb)
+        except ValueError:
+            print("Please enter a number.")
+            bet_nb = -1
+            continue
+        if bet_nb < 0:
+            print("This is a negative number.")
+        if bet_nb > 49:
+            print("This number is too high.")
+
+    bet_sum = 0
+    while bet_sum <= 0 or bet_sum > wallet:
+        print ("You have ${}".format(wallet))
+        bet_sum = input ("How much do you bet? ")
+        try:
+            bet_sum = int(bet_sum)
+        except ValueError:
+            print("Please enter a number.")
+            bet_sum = 0
+            continue
+        if bet_sum > wallet:
+            print("Your bet is higher than the money in your wallet.")
+        if bet_sum <= 0:
+            print("Please enter a positive bet.")
+        
+
+    result_nb = random.randrange(2)
+    print("The ball stopped on", result_nb, ".")
+
+    if result_nb == bet_nb:
+        wallet += 3 * bet_sum
+        print("Congratulations, perfect match. You just won", 3 * bet_sum, "$!")
+    elif result_nb % 2 == bet_nb % 2:
+        wallet += math.ceil(0.5 * bet_sum)
+        print("Not bad, color match. You just won", math.ceil(0.5 * bet_sum), "$!")
     else:
-        return 'red'
+        wallet -= bet_sum
+        print("Sorry, you lost", bet_sum, "$.")
 
-
-
-wallet = 50
-
-print ("You have ${}".format(wallet))
-bet_sum = input ("How much do you bet? ")
-bet_nb = input ("Which number do you bet on? ")
-bet_sum = int(bet_sum)
-bet_nb = int(bet_nb)
-
-result_nb = random.randrange(2)
-result_color = nb_color(result_nb)
-
-print ("The ball stopped on: {} {}".format(result_color, result_nb))
-
-if result_nb == bet_nb:
-    wallet = wallet + 4 * bet_sum
-    print('Bravo, perfect')
-elif result_color == nb_color(bet_nb):
-    print('Bravo, color match')
-else:
-    print("Perdu")
-
-print ("You now have ${}".format(wallet))
-   
+    if wallet <= 0:
+        continue_game = False
+    
+    print ("You now have ${}".format(wallet))
+    
